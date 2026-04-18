76fb17f9bc8fd4b088108d7104d2e7f6f7d07a43
bruno-dv2 <brunoge3@edu.unifor.br>
2026-04-18 14:42:01 -0300

    fix: adiciona import

--- src/main/java/com/elevomedia/elevomedia_backend/infrastructure/persistence/ClienteRepositoryJpa.java
+++ src/main/java/com/elevomedia/elevomedia_backend/infrastructure/persistence/ClienteRepositoryJpa.java
@@ -4,6 +4,7 @@ import com.elevomedia.elevomedia_backend.domain.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ClienteRepositoryJpa extends JpaRepository<Cliente, Long> {
 
